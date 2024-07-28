3d32a2b1c795596b905b3a922c99441caa82a8bb
Apfata <apfata.developer@gmail.com>
2024-07-28 13:42:44 -0500

    feat(download-lambda): finished

---
+++ dowload-lambda/app.py
@@ -0,0 +1,55 @@
+import json
+import logging
+from pathlib import Path
+
+import pandas as pd
+from business.dowload_lambdas import download_lambda_function
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/download-lambdas", methods=["POST"])
+def download_lambdas():
+    try:
+        if "file" not in request.files:
+            raise RuntimeError("No file found")
+        file = request.files["file"]
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == "":
+            return json.dumps({"error": "No selected file"}), 400
+        # Read file and convert it to pandas dataframe
+        if file:
+            df = pd.read_csv(file, header=None)
+            df = df.fillna("")
+            date_today_str = pd.Timestamp.today().strftime("%Y%m%d-%H%M")
+            current_file_path = Path(__file__).resolve().parent
+            name_date = f"business/downloads/DOWLOAD_LAMBDA_{date_today_str}.csv"
+            output_file_path = (current_file_path / name_date).resolve()
+
+        # download_lambda_functions
+        dowload_status = []
+        print("PANDAS ", df)
+        for index, row in df.iterrows():
+            print("ROW", row[0])
+            function_name = row[0]
+            success = download_lambda_function(function_name, function_name)
+            dowload_status.append({"name": function_name, "success": success})
+
+        logging.info(f"Lambda functions have been downloaded as {output_file_path}")
+        print(dowload_status)
+        df.to_csv(output_file_path, index=False)
+        return json.dumps(dowload_status), 200
+    except Exception as e:
+        logging.exception("Error trying to download", e)
+        return json.dumps({"error": str(e)}), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ dowload-lambda/business/dowload_lambdas.py
@@ -0,0 +1,59 @@
+import logging
+from pathlib import Path
+
+import boto3
+import requests
+
+
+def download_lambda_function(
+    function_name: str,
+    output_file: str,
+    profile_name: str = None,
+) -> bool:
+    try:
+        # Create a session with the specified profile, if provided
+        if profile_name:
+            session = boto3.Session(profile_name=profile_name)
+        else:
+            session = boto3.Session()
+
+        # Initialize a session using Amazon Lambda in a specific region
+        client = session.client(
+            "lambda", region_name="us-east-1"
+        )  # Ensure this is the same region as your Lambda function
+
+        # Get the function's code
+        response = client.get_function(FunctionName=function_name)
+
+        # Get the URL for the function's code
+        code_url = response["Code"]["Location"]
+
+        # Get the absolute path of the current file
+        current_file_path = Path(__file__).resolve()
+
+        # Define the output file path using pathlib
+        output_file_path = (
+            current_file_path.parent / "downloads" / f"{output_file}.zip"
+        ).resolve()
+
+        # Download the ZIP file from the URL
+        with requests.get(code_url) as r:
+            r.raise_for_status()
+            with open(output_file_path, "wb") as f:
+                f.write(r.content)
+
+        logging.info(
+            f"Lambda function {function_name} has been downloaded as {output_file_path}"
+        )
+        return True
+    except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
+        return False
+
+
+# Example usage
+if __name__ == "__main__":
+    function_name = "MyNodeJs20Function"
+    output_file = f"downloads/{function_name}.zip"
+    profile_name = None  # Replace with your AWS profile name
+    download_lambda_function(function_name, output_file, profile_name)
